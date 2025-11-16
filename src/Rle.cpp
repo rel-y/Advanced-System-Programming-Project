c915d92fb33bfc5fd749928d1ab5647c1f55a279
ofek levy <117712428+ofkelevt@users.noreply.github.com>
2025-11-16 16:33:08 +0200

    tests and empty RLE class

---
+++ src/Rle.cpp
@@ -0,0 +1,6 @@
+#include "Rle.h"
+class Rle {
+public:
+    std::string compress(std::string input);
+    std::string decompress(std::string input);
+};
---
+++ tests/decompressionTest.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "Rle.cpp"
+string decompress(std::string input); // Assume this function is defined elsewhere
+
+TEST(SaintyDecompressionTest, BasicDecompression) {
+    std::string compressedData = 1 + 'a' + 1 + 'b' + 1 + 'c';;
+    std::string expectedDecompressedData = "abc";
+
+    std::string result = decompress(compressedData);
+
+    EXPECT_EQ(result, expectedDecompressedData);
+    std::string compressedData = 3 + 'A';
+    std::string expectedDecompressedData = "AAA";
+
+    std::string result = decompress(compressedData);
+
+    EXPECT_EQ(result, expectedDecompressedData);
+}
+
+TEST(ContainsnumberTest, BackslashHandling) {
+    std::string compressedData = 3 + '3' + 4 +'6';
+    std::string expectedDecompressedData = "3336666";
+
+    std::string result = decompress(compressedData);
+
+    EXPECT_EQ(result, expectedDecompressedData);
+}
+TEST(EmptyStringDecompressionTest, EmptyInput) {
+    std::string compressedData = "";
+    std::string expectedDecompressedData = "";
+
+    std::string result = decompress(compressedData);
+
+    EXPECT_EQ(result, expectedDecompressedData);
+}
