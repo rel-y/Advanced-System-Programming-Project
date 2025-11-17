7d39cb1c795b32c60bd4f3c5655ebaf38f5e0e01
ofek levy <117712428+ofkelevt@users.noreply.github.com>
2025-11-17 13:37:42 +0200

    small changes

--- src/Rle.cpp
+++ src/Rle.cpp
@@ -4,7 +4,9 @@ std::string Rle::decompress(std::string input) {
     while(input.size() != 0) {
         int count = (int)input[0];
         char ch = input[1];
+        // Remove the first 2 characters
         input.erase(0,2);
+        // Append the charcter count amount of times
         output.append(count, ch);
     }
     return output;
