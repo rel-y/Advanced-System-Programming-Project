ef06befa8599fc4ec58a8e0156ad79fc25222ec7
rotemlewinski <rotem.lewinski@gmail.com>
2025-11-24 18:32:40 +0200

    ADV-4 i dont even know anymore

--- src/Add.cpp
+++ src/Add.cpp
@@ -2,16 +2,16 @@
 
 void Add::execute(std::string argv) {
     std::string fileName = argv.substr(0, argv.find(" "));
-    std::cout << fileName;
-    std::cout << argv;
+    //std::cout << fileName;
+    //std::cout << argv;
     if (fileName == argv) // no spaces
     {
-        std::cout << "h1";
+        //std::cout << "h1";
         return;
     }
     
     std::string text = argv.substr(argv.find(" ") + 1, argv.length() - (argv.find(" ") + 1));
-    std::cout << text;
+    //std::cout << text;
     if (text.length() == 0)
     {
         return;
@@ -20,10 +20,10 @@ void Add::execute(std::string argv) {
     std::string dirName = getenv("FolderName");
     std::string path = dirName.append("/");
     path = path.append(fileName);
-    std::cout << std::endl << path;
+    //std::cout << std::endl << path;
     std::ofstream ofs(path); // creates file
-    std::cout << std::endl << ofs.good() << std::endl;
-    std::cout << ofs.is_open();
+    //std::cout << std::endl << ofs.good() << std::endl;
+    //std::cout << ofs.is_open();
     ofs << Rle::compress(text);
 
     ofs.close();
