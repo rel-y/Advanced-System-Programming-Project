708e049854b7341281f0962a8b3c5282877c5329
rotemlewinski <rotem.lewinski@gmail.com>
2025-12-07 18:54:48 +0200

    ADV-28 fixed command line arguments parsing

--- src/python_client.py
+++ src/python_client.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    ip = args[1]
-    port = args[2]
+    ip = args[0]
+    port = int(args[1])
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create IPv4 TCP socket
     s.connect((ip, port))
