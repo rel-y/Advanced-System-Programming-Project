f1d7cdb64133c8aa6ee6e4a433382dfe7da46910
rel-y <arielo0210@gmail.com>
2025-12-08 09:33:25 +0200

    implemented the delete command, tests passed

--- src/Delete.cpp
+++ src/Delete.cpp
@@ -1,4 +1,34 @@
 #include "Delete.h"
 std::pair<int, std::string> Delete::execute(std::string name){
-    return {0, ""}; //placeholder
+    //checking the name of the file isn't empty and doens't contain any spaces
+    if(name.empty()){
+        return {400, ""};
+    }
+    if(name.find_first_of(' ') != std::string::npos){
+        return {400, ""};
+    }
+    //name of the file
+    std::filesystem::path dirName(getenv("FolderName"));
+    std::filesystem::path pathName = dirName / name;
+    
+    std::error_code ec;
+    bool exists = std::filesystem::exists(pathName, ec);
+    if (ec){ //an error occurred
+        return {500, ""}; 
+    }else if(!exists){ //the file doesn't exists
+        return {404, ""};
+    }
+    bool is_regular = std::filesystem::is_regular_file(pathName, ec);
+    if(ec){
+        return {502, ""};
+    }else if(!is_regular){
+        return {404, ""}; //the file isn't a regular file
+    }
+
+    bool removed = std::filesystem::remove(pathName, ec);
+    if(ec){ //an error occurred
+        return {500, ""};
+    }
+    //the file was deleted
+    return {204 , ""};
 }
--- src/Delete.h
+++ src/Delete.h
@@ -1,6 +1,7 @@
 #ifndef DELETE_H
 #define DELETE_H
 #include "ICommand.h"
+#include <filesystem>
 class Delete: public ICommand {
 public: 
     std::pair<int, std::string> execute(std::string argv) override;
