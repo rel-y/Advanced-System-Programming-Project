410e3f2b6df67245fcdc9371373ab6ed867b1b8e
rotemlewinski <rotem.lewinski@gmail.com>
2025-12-25 17:13:43 +0200

    ADV-48 added handlers for post, get

---
+++ src/web-server/controller/api-users-controller.js
@@ -0,0 +1,34 @@
+const singletonUsersModel = require("../model/usersModel")
+
+function postReqController(req, res) {
+    let { username, password, nickname, photo } = req.body;
+    if (!username || !password || !nickname || !photo) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify({ error: 'missing fields for signup' }));
+    }
+
+    if (!(singletonUsersModel.getUser(username) === undefined)) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify({ error: 'user with this username already exists' }));
+    }
+
+    singletonUsersModel.addUser(username, password, nickname, photo);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    return res.end(JSON.stringify({ message: 'user created successfully' }));
+}
+
+function getReqController(req, res) { // for api/users/:id !!!
+    const inputId = req.params.id;
+
+    const requestedUser = singletonUsersModel.getUser(inputId);
+
+    if (requestedUser === undefined) {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        return res.end(JSON.stringify({ error: 'user with this username does not exists' }));
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const retjson = { id: inputId, ...requestedUser };
+    return res.end(JSON.stringify(retjson));
+}
