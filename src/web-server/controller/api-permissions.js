d7fc8231ec4d7e1a53acd9c0c2ef33e9c8dd6eff
ofkelevt <root@ofek.>
2025-12-26 13:30:21 +0200

    small fixes

--- src/web-server/controller/api-permissions.js
+++ src/web-server/controller/api-permissions.js
@@ -1,13 +1,13 @@
 const {singletonMetadataModel, PermissionType, AbilityRequirement} = require("../model/metadataModel");
 
 function patchFilePermissionController(req, res){
-    const {id,pld} = req.params;
+    const {id,pid} = req.params;
     const {filePermission} = req.body;
     const {username} = req.body; // from authentication middleware
     if(username === undefined || username === null){
         return res.status(401).json({ error: "unauthorized" });
     }
-    if(id === undefined || filePermission === undefined || pld === undefined){
+    if(id === undefined || filePermission === undefined || pid === undefined){
         return res.status(400).json({ error: "bad request, missing fields" });
     }
     if(!Object.keys(PermissionType).includes(filePermission)){
@@ -18,7 +18,7 @@ function patchFilePermissionController(req, res){
         return res.status(404).json({ error: "file/folder doesn't exist" });
     }
     try{
-        singletonMetadataModel.setUserFilePermission(id, pld, PermissionType[filePermission]);
+        singletonMetadataModel.setUserFilePermission(id, pid, PermissionType[filePermission]);
         return res.status(200).json({ message: "file/folder permission updated successfully" });
     } catch (err) {
         return res.status(500).json({ error: err.message });
@@ -26,16 +26,16 @@ function patchFilePermissionController(req, res){
 }
 
 function deleteFilePermissionController(req, res){
-    const {id, pld} = req.params;
+    const {id, pid} = req.params;
     const {username} = req.body;
     if(username === undefined || username === null){
         return res.status(401).json({ error: "unauthorized" });
     }
-    if(id === undefined || pld === undefined){
+    if(id === undefined || pid === undefined){
         return res.status(400).json({ error: "bad request, missing fields" });
     }
     try{
-        singletonMetadataModel.setUserFilePermission(id, pld, PermissionType.NON);
+        singletonMetadataModel.setUserFilePermission(id, pid, PermissionType.NON);
         return res.status(200).json({ message: "user file/folder permission deleted successfully" });
     } catch (err) {
         return res.status(500).json({ error: err.message });
