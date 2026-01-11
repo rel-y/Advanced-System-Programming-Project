f90b17fae30fa5373cca22e93a218fc7a631df97
rel-y <arielo0210gmail.com>
2026-01-11 08:54:00 +0200

    added a function for action on clicking the element

--- src/react-app/src/components/file and folder list/fileListElement.js
+++ src/react-app/src/components/file and folder list/fileListElement.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './fileListElement.css'
-function ListElement({ fileName, owner = "Unable to load user", size = "-", date, location }) {
+function ListElement({ fileName, owner = "Unable to load user", size = "-", date, location, funcOnClick }) {
     if (location === 0) {
         location = "My Drive";
     }
     return (
-        <ul className="list-group list-group-horizontal lineHover-bg">
+        <ul className="list-group list-group-horizontal lineHover-bg" onClick={() => funcOnClick()}>
             <li className="p-2 list-group-item text-truncate detailBox" style={{width: "40%"}}>{fileName}</li>
             <li className="p-2 list-group-item detailBox" style={{width: "15%"}}>{date.toLocaleDateString()}</li>
-            <li className="p-2 list-group-item detailBox" style={{width: "10%"}}>{owner}</li>
+            <li className="p-2 list-group-item detailBox" style={{width: "15%"}}>{owner}</li>
             <li className="p-2 list-group-item detailBox" style={{width: "10%"}}>{size}</li>
-            <li className="p-2 list-group-item detailBox" style={{width: "15%"}}>{location}</li>
+            <li className="p-2 list-group-item detailBox" style={{width: "20%"}}>{location}</li>
         </ul>
     )
 }
