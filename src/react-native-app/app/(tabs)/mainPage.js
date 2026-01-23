10a3f926958e315d856e675eaa1c51016705c164
ofek levy <117712428+ofkelevt@users.noreply.github.com>
2026-01-23 12:48:51 +0200

    no defualt ids

--- src/react-native-app/app/(tabs)/mainPage.js
+++ src/react-native-app/app/(tabs)/mainPage.js
@@ -16,7 +16,7 @@ export default function MainPage() {
     const theme = useMemo(() => getTheme(scheme === "dark" ? "dark" : "light"), [scheme]);
     const styles = useMemo(() => createStyles(theme), [theme]);
 
-    const [idList, setIdlist] = useState(["dfce4b0f3f3ff854e6167523b63bd7fd", "e3afae6d8074b976ddc70c651f7265a4", "6e0a93c58957906679df19f30e4b0c25", "956a5a9f14d66cc346313ddba5e20ca3", "2e6f6806582e0ae84d948a740dca469d", "ec3c4cead7939b4af2800433ee796f80", "6e427941a6df938f56fa19ad4ba59a71", "f75c31651afe45e94209bae279cb8380", "4e4080c765c1b66dce579ab35383a8e9", "bf67f003cffb3ec12ec1c1f5f7347e12", "12b46cdfb5311d9cda349231314f5c1a"]);
+    const [idList, setIdlist] = useState([]);
 
     const [page, setPage] = useState("home");
 
